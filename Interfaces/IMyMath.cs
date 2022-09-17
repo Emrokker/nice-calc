805e2d8aa5c016675f4d6c9354d00a64ee9dede1
Masterflitzer <60170948+Masterflitzer@users.noreply.github.com>
2022-09-17 23:07:47 +0200

    rm c# files

--- Interfaces/IMyMath.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NiceCalc.Interfaces;
-internal interface IMyMath
-{
-    public abstract int Add(int x, int y);
-    public abstract int Subtract(int x, int y);
-    public abstract int Multiply(int x, int y);
-    public abstract int Divide(int x, int y);
-}
